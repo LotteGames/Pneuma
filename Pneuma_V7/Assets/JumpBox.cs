a2970fc9ec3d02fb729e4a544ab3f11a39d449a2
LotteGames <92548161+LotteGames@users.noreply.github.com>
2022-04-19 02:52:47 +0800

    V4-19

--- Pneuma_V7/Assets/JumpBox.cs
+++ Pneuma_V7/Assets/JumpBox.cs
@@ -36,4 +36,26 @@ public class JumpBox : MonoBehaviour
             StartCoroutine(Cat.GetComponent<CatContrl>().JumpDebug(0.3f));
         }
     }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.GetComponent<CatContrl>() != null)
+        {
+            GetComponent<Animator>().Play("Base Layer.JumpBox");
+
+            //GetComponent<Collider2D>().enabled
+            //
+            if (PowerX_Add != 0)
+            {
+                Cat.GetComponent<CatContrl>().JumpBox();
+            }
+            Cat.GetComponent<CatContrl>().CatMusic.PlayMusic(0);
+            Cat.GetComponent<Rigidbody2D>().velocity = new Vector3(0, 0, 0);
+            Cat.GetComponent<Rigidbody2D>().AddForce(new Vector3(collision.GetComponent<CatContrl>().JumpPower * PowerX_Add, collision.GetComponent<CatContrl>().JumpPower * PowerY_Add, 0));
+            Cat.GetComponent<CatContrl>().NowCatAct = CatContrl.CatAct.Jump;
+            Cat.GetComponent<CatContrl>().CanJump = false;//不能繼續跳
+            Cat.GetComponent<CatContrl>().CanLong = true;//可以延長
+            StartCoroutine(Cat.GetComponent<CatContrl>().JumpDebug(0.3f));
+        }
+    }
 }
